f4080ec3e25bdd507f032e47244ea1adaefad9bf
Harshil <harshilk27@gmail.com>
2023-07-09 21:47:11 +0530

    CRUD API using Function Based View in DRF

---
+++ gs3/App/views.py
@@ -0,0 +1,83 @@
+from django.shortcuts import render
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import io 
+
+
+# CRUD API Using Function Based View
+@csrf_exempt
+def student_api(request):
+    if request.method == "GET":
+        json_data = request.body
+        print(json_data)
+        stream = io.BytesIO(json_data)
+        pythondata = JSONParser().parse(stream)
+        id = pythondata.get("id", None)
+        if id is not None:
+            stu = Student.objects.get(id=id)
+            serializer = StudentSerializer(stu)
+            return JsonResponse(serializer.data)
+        
+        stu = Student.objects.all()
+        serializer = StudentSerializer(stu, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+
+    if request.method == "POST":
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        pythondata = JSONParser().parse(stream)
+        serializer = StudentSerializer(data=pythondata)
+        if serializer.is_valid():
+            serializer.save()
+            response = {"msg": "Data Successfully Created!!"}
+            return JsonResponse(response)
+
+        return JsonResponse(serializer.errors) 
+    
+
+    if request.method == "PUT":
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        pythondata = JSONParser().parse(stream)
+        id = pythondata.get("id")
+        stu = Student.objects.get(id=id)
+        # Fully Updated
+        serializer = StudentSerializer(stu, data=pythondata)
+        if serializer.is_valid():
+            serializer.save()
+            res = {"msg": "Data Fully Updated!!!"}
+            return JsonResponse(res)
+        
+        return JsonResponse(serializer.errors)
+    
+
+    if request.method == "PATCH":
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        pythondata = JSONParser().parse(stream)
+        id = pythondata.get("id")
+        stu = Student.objects.get(id=id)
+        # Partially Updated
+        serializer = StudentSerializer(stu, data=pythondata, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            res = {"msg": "Data Partialy Updated!!!"}
+            return JsonResponse(res)
+        
+        return JsonResponse(serializer.errors)
+
+
+    if request.method == "DELETE":
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        pythondata = JSONParser().parse(stream)
+        id = pythondata.get("id")
+        stu = Student.objects.get(id=id).delete()
+        res = {"msg": "Data Deleted!!!"}
+        return JsonResponse(res)
+
