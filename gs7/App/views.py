9cd00b75d173d815f128da8d4cbcd6b3e86e1fe9
Harshil <harshilk27@gmail.com>
2023-07-12 23:16:19 +0530

    Implementation of Function Based(@api_view) CRUD API in DRF

---
+++ gs7/App/views.py
@@ -0,0 +1,46 @@
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+
+@api_view(http_method_names=["GET", "POST", "PUT", "PATCH", "DELETE"])
+def student_crud_api(request, id=None):
+    if request.method == "GET":
+        if id is not None:
+            stu = Student.objects.get(id=id)
+            serializer = StudentSerializer(stu)
+            return Response(serializer.data)
+        
+        stu = Student.objects.all()
+        serializer = StudentSerializer(stu, many=True)
+        return Response(serializer.data)
+
+    if request.method == "POST":
+        print("Data:", request.data)
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"msg":"Data Successfully Created!!!"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors)
+    
+    if request.method == "PUT":
+        stu = Student.objects.get(id=id)
+        serializer = StudentSerializer(stu, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"msg": "Data Updated Successfully!!!"})
+        return Response(serializer.errors)
+
+    if request.method == "PATCH":
+        stu = Student.objects.get(id=id)
+        serializer = StudentSerializer(stu, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"msg": "Data Updated Partially Successfully!!!"})
+        return Response(serializer.errors)
+    
+    if request.method == "DELETE":
+        stu = Student.objects.get(id=id)
+        stu.delete()
+        return Response({"msg":"Data Deleted!!!"})
---
+++ gs7/gs7/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from App import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("studentapi/", views.student_crud_api),
+    path("studentapi/<int:id>/", views.student_crud_api)
+]
